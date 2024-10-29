e384062432611aed7111544da31f6f523d4a8f29
Marco Mechini <mechinimarco35@gmail.com>
2024-10-29 15:35:05 +0100

    feat: inserimento valore di stop fizzbuzz aggiunto do while per riprovare

--- js/script.js
+++ js/script.js
@@ -1,15 +1,42 @@
-for (let i = 1; i <= 100; i++) {
+let flag = false;
 
-    if (i % 5 == 0 && i % 3 == 0) {
-        console.log(i + ' FizzBuzz');
+do {
 
-    } else if (i % 5 == 0) {
-        console.log(i + ' Buzz');
+    //Inserimento valore dall'utente
+    const counterFizzBuzz = parseInt(prompt("Inserisci il numero fino a dove vuoi avere FizzBuzz"))
 
-    } else if (i % 3 == 0) {
-        console.log(i + ' Fizz');
+    //Controllo inserimento valore
+    if (isNaN(counterFizzBuzz) || counterFizzBuzz == null) {
+        console.log("Il valore inserito non è corretto perfavore inserite un numero");
+    } else {
+
+        for (let i = 1; i <= counterFizzBuzz; i++) {
+
+            if (i % 5 == 0 && i % 3 == 0) {
+                console.log(i + ' FizzBuzz');
+
+            } else if (i % 5 == 0) {
+                console.log(i + ' Buzz');
+
+            } else if (i % 3 == 0) {
+                console.log(i + ' Fizz');
+
+            } else {
+                console.log(i);
+            }
+        }
+    }
+
+    //variabile per far scegliere se l'utente vuole continuare
+    let loop = prompt("Inserisci Y per continuare oppure N per smettere")
+
+    if (loop == "Y" || loop == "y") {
+        flag = true;
 
     } else {
-        console.log(i);
+        flag = false
     }
-}   
+
+} while (flag);
+
+
